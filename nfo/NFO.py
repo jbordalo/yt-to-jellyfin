a37a6e182e4e2d7c5ff8a98399966c35c9ff8e2a
jbordalo <j.bordalo@campus.fct.unl.pt>
2025-08-08 00:39:08 +0000

    Sanitize nfo files

--- nfo/NFO.py
+++ nfo/NFO.py
@@ -16,14 +16,35 @@ class VideoNFOWriter:
         self.episodes.append((title, description, upload_date))
 
 
-    def __main_nfo(self) -> str:
+    def _escape_nfo_text(self, text: str) -> str:
+        if text is None:
+            return ""
+        replacements = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            "'": "&apos;",
+            '"': "&quot;",
+            "\n": "<br/>",
+        }
+
+        for k, v in replacements.items():
+            text = text.replace(k, v)
+
+        return text
+    
+
+
+    def _main_nfo(self) -> str:
         return show_template.format(title=self.title)
 
 
     def write(self):
-        with open(os.path.join(self.base_path, self.safe_title, "tvshow.nfo"), "w") as f:
-            f.write(self.__main_nfo())
+        #with open(os.path.join(self.base_path, self.safe_title, "tvshow.nfo"), "w") as f:
+        #    f.write(self._main_nfo())
 
         for number, (title, description, upload_date) in enumerate(self.episodes, 1): 
+            title = self._escape_nfo_text(title)
+            description = self._escape_nfo_text(description)
             with open(os.path.join(self.base_path, self.safe_title, f"dload-{number:03d}.nfo"), "w") as f:
                 f.write(episode_template.format(title=title, description=description, number=number, date=upload_date))
